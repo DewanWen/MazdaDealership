12c4842c31345bf9c44485bb6812a29779c88ad0
Dewan <dewan@aboutairpollution.co.za>
2022-12-05 23:03:24 +0200

    First Commit

---
+++ MazdaDealership/Data Access/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazdaDealership.Data_Access
+{
+    internal class Class1
+    {
+    }
+}
